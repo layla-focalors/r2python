562a16895cbb99d87c8f0181326d271502f58b81
arisia <starlights2283@gmail.com>
2023-08-26 14:14:39 +0900

    add install.py

---
+++ environment/install.py
@@ -0,0 +1,8 @@
+# 1회 실행, 자동 패키지 설치
+import os
+packages = ['numpy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn', 'scipy', 'statsmodels', 'stemgraphic']
+
+for i in range(len(packages)):
+    install = f'pip install {packages[i]}'
+    os.system(install)
+    print("now installing " + packages[i] + "...")
